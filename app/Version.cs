db1a66b0ec75c197fe3b4d175a53635126bc3ae5
harrsn <64714607+Harrsn@users.noreply.github.com>
2023-09-03 21:45:52 -0400

    Add files via upload

---
+++ app/Version.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using DHT.Utils;
+
+[assembly: AssemblyVersion(Version.Tag)]
+[assembly: AssemblyFileVersion(Version.Tag)]
+[assembly: AssemblyInformationalVersion(Version.Tag)]
+
+namespace DHT.Utils; 
+
+static class Version {
+	public const string Tag = "37.2.0.0";
+}
